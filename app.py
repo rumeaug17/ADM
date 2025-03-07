a10b647ce7447dd8d82f326a8dd5aa55c4389b00
Rumeau Guillaume <rumeaug17@gmail.com>
2025-03-07 09:43:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import json
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 DATA_FILE = "applications.json"
