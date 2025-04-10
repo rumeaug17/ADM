2bdd8159a3bd30b6b4d0a6a3976efa1ecefc4db1
Rumeau Guillaume <rumeaug17@gmail.com>
2025-04-10 11:42:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def app_to_dict(app_obj: Application) -> dict:
         "possession": app_obj.possession.isoformat() if app_obj.possession else None,
         "type_app": app_obj.type_app,
         "hosting": app_obj.hosting,
-        "criticite": app_obj.criticite,
+        "criticite": str(app_obj.criticite) if app_obj.criticite is not None else None,
         "disponibilite": app_obj.disponibilite,
         "integrite": app_obj.integrite,
         "confidentialite": app_obj.confidentialite,
