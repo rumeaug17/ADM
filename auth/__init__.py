684bc40e1530562b5c02fa0180c334d133631439
Rumeau Guillaume <rumeaug17@gmail.com>
2025-05-27 16:55:50 +0200

    Update __init__.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,7 +1,8 @@
 from auth.mysql_backend import MySQLAuthBackend
 
 def get_auth_backend(config):
-    if config["auth_backend"] == "mysql":
-        return MySQLAuthBackend(config["DB_CONNECTION"])
+    auth_backend = config.get("auth_backend", "mysql").lower()
+    if auth_backend == "mysql":
+        return MySQLAuthBackend(config["auth_sql_connection_url"])
     else:
         raise ValueError("Auth Backend inconnu ou non configuré")
